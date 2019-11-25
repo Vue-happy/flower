95541ef4747e0958ebf2e21086031c06a3ddacb8
Mr.Tan <oncwnuKHgCucflJ5YDjM-GP5MZA8@git.weixin.qq.com>
2019-11-25 20:32:59 +0800

    路由搭建完成

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div class="router">
+  <router-link to="/home">首页</router-link>
+  <router-link to="/sort">分类</router-link>
+  <router-link to="/cart">购物车</router-link>
+  <router-link to="/profile">我的</router-link>
+  <router-link to="/login">登陆</router-link>
+
+  <router-view></router-view>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  name: 'app'
+
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus" scoped>
+
+ .router
+    position fixed
+    bottom 0
+    left 0
+    font-size 20px
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
 Vue.config.productionTip = false
 
+import Home from '../src/pages/Home'
+import Sort from '../src/pages/Sort'
+import Cart from '../src/pages/Cart'
+import Profile from '../src/pages/Profile'
+import Login from '../src/pages/Login'
+
+//Vue全局注册组件的语法:Vue
+Vue.component('Home',Home)
+Vue.component('Sort',Sort)
+Vue.component('Cart',Cart)
+Vue.component('Profile',Profile)
+Vue.component('Login',Login)
+
 new Vue({
   render: h => h(App),
+  router, //注册路由
+  // store
 }).$mount('#app')
