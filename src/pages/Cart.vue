3e30e1cc69b8947b110a6edb6671b3d805518e17
king <1441922245@qq.com>
2019-11-25 22:21:27 +0800

    1

--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -1,14 +1,25 @@
 <template>
-  <div>
-    profile
-  </div>
+    <header class="headerbar">
+    <div class="headerbar-left"></div>
+      <div class="headerbar-center">
+          <p class="header-content">购物车</p>
+      </div>
+      <div class=""></div>
+    </header>
 </template>
-
 <script>
 
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
+<style lang="stylus" scoped>
+  .headerbar {
+      display :flex
+      background-color  : yellowgreeen 
+      // width 100%
+      height 15px
+      line-height 15px
+      line-height 15px
+      
+  }
 </style>
