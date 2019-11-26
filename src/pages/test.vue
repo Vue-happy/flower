3d951876a34e79f641d13ada0b7c5e8f78e3c433
tsstnt@163.com <52000212+tsstnt@users.noreply.github.com>
2019-11-27 01:20:32 +0800

    封装完毕  单击回到顶部(

--- src/pages/test.vue
+++ src/pages/test.vue
@@ -1,69 +1,78 @@
 <template>
-  <div class="box1"> 
-    <div class="box2">
-    </div>
+  <div class="box1">
+    <div class="box2"></div>
 
-  <div id="boxFixed" v-show="isFixed">  
-        自闭boy
+    <div id="boxFixed" v-show="isFixed" @click="toTop">
+      自闭boy
+    </div>
   </div>
-</div>
 </template>
   
 <script type="text/ecmascript-6">
-  export default {
-    data(){
-      
-      return{
-      isFixed:false,
-    
-    }
-},  
-  methods: {
-    handleScroll(){
-      let that = this;
-    let scrollTop = window.pageYOffset||document.documentElement.scrollTop || document.body.scrollTop
-    let clientHeight = document.documentElement.clientHeight
-    // if (scrollTop > clientHeight) {
-    //   that.isFixed = true
-    // }else{
-    //   that.isFixed = false
-    // }
-    that.isFixed = scrollTop > clientHeight ? true : false
-      console.log(that.isFixed)
-    
-    // console.log(1)
-    console.log(document.documentElement.scrollTop,document.body.scrollTop,window.pageYOffset)
-}
+export default {
+  data() {
+    return {
+      isFixed: false
+    };
   },
-  mounted(){
+  methods: {
+    handleScroll() {
+      let that = this;
+      let scrollTop =
+        window.pageYOffset ||
+        document.documentElement.scrollTop ||
+        document.body.scrollTop;
+      let clientHeight = document.documentElement.clientHeight;
+      // if (scrollTop > clientHeight) {
+      //   that.isFixed = true
+      // }else{
+      //   that.isFixed = false
+      // }
+      that.isFixed = scrollTop > clientHeight ? true : false;
+      console.log(that.isFixed);
+
+      // console.log(1)
+      console.log(
+        document.documentElement.scrollTop,
+        document.body.scrollTop,
+        window.pageYOffset
+      );
+    },
+    toTop(){
+      console.log(111)
+      window.pageYOffset = document.documentElement.scrollTop = document.body.scrollTop =0;
+
+        
+    }
+  },
+  mounted() {
     let that = this;
-    that.$nextTick(function(){
-
-      window.addEventListener('scroll',that.handleScroll) 
-    })
-},
-
+    that.$nextTick(function() {
+      window.addEventListener("scroll", that.handleScroll);
+    });
   }
+};
 </script>
   
 <style lang="stylus" rel="stylesheet/stylus">
-  .box1
-    width 300px
-    height 700px
-    // background pink
-    .box2
-      height 1500px
-    #boxFixed
-      width 50px
-      height 50px
-      line-height 50px
-      border-radius 50%
-      background pink
-      position fixed
-      top 85%
-      right 0
+.box1 {
+  width: 300px;
+  height: 700px;
 
+  // background pink
+  .box2 {
+    height: 1500px;
+  }
 
-
-      
+  #boxFixed {
+    width: 50px;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 50%;
+    background: pink;
+    position: fixed;
+    top: 85%;
+    right: 0;
+  }
+}
 </style>
