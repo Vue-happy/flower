19bf839d414f7553e4e5cc2303198d49cdacb3b9
Mr.Tan <oncwnuKHgCucflJ5YDjM-GP5MZA8@git.weixin.qq.com>
2019-11-27 09:32:42 +0800

    done

---
+++ src/components/Detail/Detail.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="detail">
+    <div class="headerbar"></div>
+    <nav class="filter"></nav>
+    <nav class="purpose"></nav>
+    <section class="product"></section>
+    <div class="scrollbox"></div>
+    <footer class="scrollbox"></footer>
+  </div>
+</template>
+  
+<script type="text/ecmascript-6">
+  export default {
+  
+  }
+</script>
+  
+<style lang="stylus" rel="stylesheet/stylus">
+  #detail
+    width 375px
+    .filter
+      background pink 
+      height 44px
+
+</style>
