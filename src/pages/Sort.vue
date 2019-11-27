24647ba05d48432a2d1706c5563705bf9f804d78
= <=>
2019-11-27 20:42:39 +0800

    8:42

--- src/pages/Sort.vue
+++ src/pages/Sort.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
-    sort
     
   </div>
 </template>
 
 <script>
-
+export default {
+  
+}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
+<style lang="">
+  
 </style>
